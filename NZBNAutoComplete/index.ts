cd7a090ff67e2f81c1f672aeabd5c915ff1264a1
unknown <gareth.cheyne@nz.harveynorman.com>
2020-08-28 09:53:22 +1200

    master

--- NZBNAutoComplete/index.ts
+++ NZBNAutoComplete/index.ts
@@ -1,33 +1,32 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
 export class NZBNAutoComplate implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
-	private localNotifyOutputChanged: () => void;
-	private context: ComponentFramework.Context<IInputs>;
-	private container: HTMLDivElement;
-	private refreshData: EventListenerOrEventListenerObject;
+    private localNotifyOutputChanged: () => void;
+    private context: ComponentFramework.Context<IInputs>;
+    private container: HTMLDivElement;
+    private refreshData: EventListenerOrEventListenerObject;
 
-	// input element that is used to create the autocomplete
-	private inputElement: HTMLInputElement;
+    // input element that is used to create the autocomplete
+    private inputElement: HTMLInputElement;
 
-	// Datalist element.
-	private datalistElement: HTMLDataListElement;
+    // Datalist element.
+    private datalistElement: HTMLDataListElement;
 
-	private _nzbnToken: string | null;
+    private _nzbnToken: string | null;
 
-	private _nzbnNumber: string;
-	private _companyName: string;
-	private _tradingAs: string;
-	private _statusCode: string;
-	private _statusReason: string;
-	private _registrationDate: Date;
-	private _bicCode: string;
-	private id: string;	
+    private _nzbnNumber: string;
+    private _companyName: string;
+    private _tradingAs: string;
+    private _statusCode: string;
+    private _statusReason: string;
+    private _registrationDate: Date;
+    private _bicCode: string;
+    private id: string;
 
-	constructor()
-	{
+    constructor() {
 
-	}
+    }
 
 	/**
 	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
@@ -37,199 +36,194 @@ export class NZBNAutoComplate implements ComponentFramework.StandardControl<IInp
 	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
 	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
 	 */
-	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
-	{
-		this.localNotifyOutputChanged = notifyOutputChanged;
-		this.context = context;
-		this._nzbnToken = context.parameters.nzbnToken.raw;
+    public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
+        this.localNotifyOutputChanged = notifyOutputChanged;
+        this.context = context;
+        this._nzbnToken = context.parameters.nzbnToken.raw;
 
-		// @ts-ignore
-		this.id = context.parameters.value.attributes.LogicalName;
+        // @ts-ignore
+        this.id = context.parameters.value.attributes.LogicalName;
 
-		// Assinging enviroment vairable.
-		this.context = context;
-		this.container = document.createElement("div");
-		this.inputElement = document.createElement("input");
-		this.inputElement.name = "autocomplete_" + this.id
-		this.inputElement.placeholder = "---";
-		this.inputElement.autocomplete = "off";
-		this.inputElement.className = "pcfCustomField"
-		this.inputElement.setAttribute("list", "list_" + this.id);
+        // Assinging enviroment vairable.
+        this.context = context;
+        this.container = document.createElement("div");
+        this.inputElement = document.createElement("input");
+        this.inputElement.name = "autocomplete_" + this.id
+        this.inputElement.placeholder = "---";
+        this.inputElement.autocomplete = "off";
+        this.inputElement.className = "pcfCustomField"
+        this.inputElement.setAttribute("list", "list_" + this.id);
 
 
-		// Get initial values from field.
-		// @ts-ignore
-		this.inputElement.value = this.context.parameters.value.formatted
+        // Get initial values from field.
+        // @ts-ignore
+        this.inputElement.value = this.context.parameters.value.formatted
 
-		// Add an eventlistner the element and bind it to a  function.
-		this.inputElement.addEventListener("input", this.getSuggestions.bind(this));
-						
-		// creating HTML elements for data list 
-		this.datalistElement = document.createElement("datalist");
-		this.datalistElement.id = "list_" + this.id;
+        // Add an eventlistner the element and bind it to a  function.
+        this.inputElement.addEventListener("input", this.getSuggestions.bind(this));
 
-		var optionsHTML = "";
+        // creating HTML elements for data list 
+        this.datalistElement = document.createElement("datalist");
+        this.datalistElement.id = "list_" + this.id;
 
-		//@ts-ignore 
-		this.datalistElement.innerHTML = optionsHTML;
-					
-		// Appending the HTML elements to the control's HTML container element.
-		// Add input element
-		this.container.appendChild(this.inputElement);
+        var optionsHTML = "";
 
-		//Add datalist element
-		this.container.appendChild(this.datalistElement);
-		container.appendChild(this.container);	
-	}
+        //@ts-ignore 
+        this.datalistElement.innerHTML = optionsHTML;
+
+        // Appending the HTML elements to the control's HTML container element.
+        // Add input element
+        this.container.appendChild(this.inputElement);
+
+        //Add datalist element
+        this.container.appendChild(this.datalistElement);
+        container.appendChild(this.container);
+    }
 
 
 	/**
 	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
 	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
 	 */
-	public updateView(context: ComponentFramework.Context<IInputs>): void
-	{
-		((this._companyName != undefined) ? this.inputElement.value = this._companyName : null)
-	}
+    public updateView(context: ComponentFramework.Context<IInputs>): void {
+        ((this._companyName != undefined) ? this.inputElement.value = this._companyName : null)
+    }
 
-	public getSuggestions(evt: Event) {
+    public getSuggestions(evt: Event) {
 
-		// Connect to an API and get the suggesstion as user key presses and update dropdown.
-		let key = "NZBN"
-		let input = (this.inputElement.value as any) as string;
-		if (input.indexOf(key) == -1 && input.length > 0)
-		{
-			this.datalistElement.innerHTML = "";
-			let query = "entities?search-term=" + input + "&page-size=20";
-			let options = {
-				host: 'api.business.govt.nz/services/v4/nzbn/',
-				path: query,
-				headers: {
-					'accept': 'application/json',
-					'authorization': "Bearer " + this._nzbnToken
-				}
-			}
-			const https = require('https');
+        // Connect to an API and get the suggesstion as user key presses and update dropdown.
+        let key = "NZBN"
+        let input = (this.inputElement.value as any) as string;
+        if (input.indexOf(key) == -1 && input.length > 0) {
+            this.datalistElement.innerHTML = "";
+            let query = "entities?search-term=" + encodeURIComponent(input) + "&page-size=20";
+            let options = {
+                host: 'api.business.govt.nz/services/v4/nzbn/',
+                path: query,
+                headers: {
+                    'accept': 'application/json',
+                    'authorization': "Bearer " + this._nzbnToken
+                }
+            }
+            const https = require('https');
 
-			https.get(options, (resp: any) => {
-				let data = '';			
-				// A chunk of data has been recieved.
-				resp.on('data', (chunk: any) => {
-				data += chunk;
-				});		  
-				// The whole response has been received. Print out the result.
-				resp.on('end', () => {
-					var response = JSON.parse(data);
-					console.log(response);
-					var optionsHTML = "";
-					var optionsHTMLArray = new Array();
-					for (var i = 0; i < response.items.length; i++) {
-					// Build the values for the AutoComplete Array and Add ID for after select use.
-						var lastTradingName = ((response.items[i].tradingNames.length > 0) ? this.titleCase(response.items[i].tradingNames[0].name) : this.titleCase(response.items[i].entityName));
+            https.get(options, (resp: any) => {
+                let data = '';
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk: any) => {
+                    data += chunk;
+                });
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    var response = JSON.parse(data);
+                    console.log(response);
+                    var optionsHTML = "";
+                    var optionsHTMLArray = new Array();
+                    for (var i = 0; i < response.items.length; i++) {
+                        // Build the values for the AutoComplete Array and Add ID for after select use.
+                        var lastTradingName = ((response.items[i].tradingNames.length > 0) ? this.titleCase(response.items[i].tradingNames[0].name) : this.titleCase(response.items[i].entityName));
 
-						optionsHTMLArray.push('<option value="');
-						optionsHTMLArray.push(this.titleCase(response.items[i].entityName) + ". NZBN: " + response.items[i].nzbn);
-						optionsHTMLArray.push('">  Status: ' + response.items[i].entityStatusDescription + ', T/A: ' + lastTradingName + '</option>');						
-					}
-					this.datalistElement.innerHTML = optionsHTMLArray.join("");  
-					this.localNotifyOutputChanged  
-				});
+                        optionsHTMLArray.push('<option value="');
+                        optionsHTMLArray.push(this.titleCase(response.items[i].entityName) + ". NZBN: " + response.items[i].nzbn);
+                        optionsHTMLArray.push('">  Status: ' + response.items[i].entityStatusDescription + ', T/A: ' + lastTradingName + '</option>');
+                    }
+                    this.datalistElement.innerHTML = optionsHTMLArray.join("");
+                    this.localNotifyOutputChanged
+                });
 
-			}).on("error", (err: { message: string; }) => {
-				console.log("Error: " + err.message);
-			});		
-		}
-		else {
-			this.getDetails(this.inputElement.value)
-		}		
-	}
+            }).on("error", (err: { message: string; }) => {
+                console.log("Error: " + err.message);
+            });
+        }
+        else {
+            this.getDetails(this.inputElement.value)
+        }
+    }
 
-	getDetails(value: string){
-		// set the key to lok for in the input that was placed above.
-		let key = "NZBN"
-		if (value.indexOf(key) > -1){
-			let _nzbn = value.substring(value.indexOf(key) + 6, value.length);
+    getDetails(value: string) {
+        // set the key to lok for in the input that was placed above.
+        let key = "NZBN"
+        if (value.indexOf(key) > -1) {
+            let _nzbn = value.substring(value.indexOf(key) + 6, value.length);
 
-			var query = "entities/" + _nzbn;
-			var options = {
-				host: 'api.business.govt.nz/services/v4/nzbn/',
-				path: query,
-				headers: {
-					'accept': 'application/json',
-					'authorization': "Bearer " + this._nzbnToken
-				}
-			}
-	
-			const https = require('https');
+            var query = "entities/" + _nzbn;
+            var options = {
+                host: 'api.business.govt.nz/services/v4/nzbn/',
+                path: query,
+                headers: {
+                    'accept': 'application/json',
+                    'authorization': "Bearer " + this._nzbnToken
+                }
+            }
 
-			https.get(options, (resp: any) => {
-				let data = '';
-				
-				// A chunk of data has been recieved.
-				resp.on('data', (chunk: any) => {
-				  data += chunk;
-				});
-			  
-				// The whole response has been received. Print out the result.
-				resp.on('end', () => {
-					var response = JSON.parse(data);
-					console.log(response)
-					this._nzbnNumber = response.nzbn
-					this._companyName = this.titleCase(response.entityName)
-					this._tradingAs = (( !Array.isArray(response.tradingNames) || !response.tradingNames.length ) ?  this.titleCase(response.entityName) : this.titleCase(response.tradingNames[0].name))
-					this._statusCode = response.entityStatusCode
-					this._statusReason = response.entityStatusDescription
-					this._registrationDate = response.registrationDate
-					this._bicCode = (( !Array.isArray(response.industryClassifications) || !response.industryClassifications.length ) ? null : response.industryClassifications[0].classificationCode)
-					this.localNotifyOutputChanged();
+            const https = require('https');
 
-				});
+            https.get(options, (resp: any) => {
+                let data = '';
 
-			}).on("error", (err: { message: string; }) => {
-				console.log("Error: " + err.message);
-			});		
-		}
-		else {
-			console.log("setSelected :: No " + key);
-			return {};
-		}	
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk: any) => {
+                    data += chunk;
+                });
 
-	}
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    var response = JSON.parse(data);
+                    console.log(response)
+                    this._nzbnNumber = response.nzbn
+                    this._companyName = this.titleCase(response.entityName)
+                    this._tradingAs = ((!Array.isArray(response.tradingNames) || !response.tradingNames.length) ? this.titleCase(response.entityName) : this.titleCase(response.tradingNames[0].name))
+                    this._statusCode = response.entityStatusCode
+                    this._statusReason = response.entityStatusDescription
+                    this._registrationDate = response.registrationDate
+                    this._bicCode = ((!Array.isArray(response.industryClassifications) || !response.industryClassifications.length) ? null : response.industryClassifications[0].classificationCode)
+                    this.localNotifyOutputChanged();
 
-	public titleCase(s: string) {
-		let str = s.toLowerCase().split(' ');
-		let final = [];
-		for (let word of str) {
-			final.push(word.charAt(0).toUpperCase() + word.slice(1));
-		}
-		return final.join(' ');
-	}
+                });
+
+            }).on("error", (err: { message: string; }) => {
+                console.log("Error: " + err.message);
+            });
+        }
+        else {
+            console.log("setSelected :: No " + key);
+            return {};
+        }
+
+    }
+
+    public titleCase(s: string) {
+        let str = s.toLowerCase().split(' ');
+        let final = [];
+        for (let word of str) {
+            final.push(word.charAt(0).toUpperCase() + word.slice(1));
+        }
+        return final.join(' ');
+    }
 
 	/** 
 	 * It is called by the framework prior to a control receiving new data. 
 	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
 	 */
-	public getOutputs(): IOutputs
-	{
+    public getOutputs(): IOutputs {
 
-		return {
-			value: this._companyName,
-			nzbnNumber: this._nzbnNumber,
-			companyName: this._companyName,
-			tradingAs: this._tradingAs,
-			statusCode: this._statusCode,
-			statusReason: this._statusReason,
-			registrationDate: this._registrationDate,
-			bicCode: this._bicCode
-		};
-	}
+        return {
+            value: this._companyName,
+            nzbnNumber: this._nzbnNumber,
+            companyName: this._companyName,
+            tradingAs: this._tradingAs,
+            statusCode: this._statusCode,
+            statusReason: this._statusReason,
+            registrationDate: this._registrationDate,
+            bicCode: this._bicCode
+        };
+    }
 
 	/** 
 	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
 	 * i.e. cancelling any pending remote calls, removing listeners, etc.
 	 */
-	public destroy(): void
-	{
-		// Add code to cleanup control if necessary
-	}
+    public destroy(): void {
+        // Add code to cleanup control if necessary
+    }
 }
